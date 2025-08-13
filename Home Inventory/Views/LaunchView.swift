274beb571263ae227af93338e534d7989242c14b
custom_software <kcluff50@gmail.com>
2025-08-13 07:56:50 -0600

    Adding launch view

---
+++ Home Inventory/Views/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Home Inventory
+//
+//  Created by Kenneth Cluff on 8/13/25.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
